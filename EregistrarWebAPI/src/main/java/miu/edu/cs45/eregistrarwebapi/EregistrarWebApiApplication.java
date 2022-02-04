cb4cd61bc06056fc0942a906449002117360b4af
EliltaW <eliltawondimu12@gmail.com>
2022-02-03 19:51:51 -0600

    lab 12

---
+++ EregistrarWebAPI/src/main/java/miu/edu/cs45/eregistrarwebapi/EregistrarWebApiApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.cs45.eregistrarwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EregistrarWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EregistrarWebApiApplication.class, args);
+	}
+
+}
